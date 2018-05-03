90c3cf6a67af63b0c35d1210b4d681e0a4e65006
Grant Harris <grant@grantharris.ca>
2018-05-02 23:25:39 -0700

    Some more work with the playback functionality

--- src/Playback.cpp
+++ src/Playback.cpp
@@ -1,86 +1,153 @@
 #include "Playback.h"
-#include "leveldb/db.h"
 
+/**
+ * @brief [brief description]
+ * @details [long description]
+ * 
+ * @param file_name Location of the leveldb directory
+ */
 Playback::Playback(std::string file_name){
     leveldb::Options options;
     options.create_if_missing = true;
     leveldb::Status status = leveldb::DB::Open(options, file_name, &db);
-    if (!s.ok()) BOSST_LOG_TRVIAL(error) << s.ToString();
+    if (!status.ok()) BOSST_LOG_TRVIAL(error) << status.ToString();
+
+    //eventually load from config/saved state
+    playhead = "0";
+    current_state = PlaybackState::STOPPED;
 }
 
-void Playback::cue_record(){
-    this->set_state(State.CUE_RECORD);
-}
-
+/**
+ * @brief Starts recording
+ * @details [long description]
+ */
 void Playback::record(){
-    if (current_state == State.RECORDING){
+    if (current_state == PlaybackState::RECORDING){
         return;
     }
-    this->set_state(State.RECORDING);
-}
-
-void Playback::loop(bool t_loop){
-    loop = t_loop
-}
-
-void Playback::play(){
-    if (current_state == State.PLAYING){
-        return;
-    }
-
-    if(current_state == State.STOPPED){
+    
+    if(current_state == PlaybackState::STOPPED){
         //start new playback thread
-        thread = new boost::thread(boost::bind(&Playback::play_loop, this));
+        record_thread = new boost::thread(boost::bind(&Playback::record_loop, this));
+        start_time = std::chrono::steady_clock::now();
     }
 
-    this->set_state(State.PLAYING);
-    pause_condition_variable.notify_one();
+    this->set_state(PlaybackState::RECORDING);
 }
 
+/**
+ * @brief Sets if playback should loop on end
+ * @details [long description]
+ * 
+ * @param t_loop boolean
+ */
+void Playback::toggle_loop(bool t_loop){
+    loop = t_loop;
+}
+
+/**
+ * @brief Starts playback
+ * @details [long description]
+ */
+void Playback::play(){
+    if (current_state == PlaybackState::PLAYING){
+        return;
+    }
+
+    if(current_state == PlaybackState::STOPPED){
+        //start new playback thread
+        playback_thread = new boost::thread(boost::bind(&Playback::play_loop, this));
+    }
+
+    this->set_state(PlaybackState::PLAYING);
+}
+
+/**
+ * @brief Pauses playback
+ * @details [long description]
+ */
 void Playback::pause(){
-    this->set_state(State.PAUSED);
+    this->set_state(PlaybackState::PAUSED);
 }
 
+/**
+ * @brief Stops playback
+ * @details [long description]
+ */
 void Playback::stop(){
-    this->set_state(State.STOPPED);
+    if (current_state == PlaybackState::RECORDING){
+        record_thread->join();
+    }
+    if (current_state == PlaybackState::PLAYING){
+        playback_thread->join();
+    }
+    this->set_state(PlaybackState::STOPPED);
+    index.playhead = 0;
 }
 
-void Playback::set_state(State state){
-    boost::unique_lock<boost::mutex> lock(the_mutex);
-    current_state = state
+/**
+ * @brief Set the current state of playback. eg Playing
+ * @details [long description]
+ * 
+ * @param state [description]
+ */
+void Playback::set_state(PlaybackState state){
+    boost::unique_lock<boost::mutex> lock(state_mutex);
+    current_state = state;
     lock.unlock();
 }
-void Playback::next(){
 
-}
-
-State Playback::get_state(){
-    return current_state
-}
-
-unsigned int Playback::get_index(){
-
-}
-
-unsigned int Playback::get_total(){
-
-}
-
-double Playback::get_size(unsigned int index){
-
-}
 
 void Playback::register_update_fn(std::function<void(std::vector<Pixel>&)> t_callback){
     callback = t_callback;
 }
 
-void Playback::play_loop()_{
-    boost::unique_lock<boost::mutex> lock(the_mutex);
+/**
+ * @brief Record loop function
+ * @details [long description]
+ */
+void Playback::record_loop(){
+    boost::unique_lock<boost::mutex> lock(frame_mutex);
+    while(current_state == PlaybackState::RECORDING)
+    {
+
+        while(frame_queue.empty())
+        {
+            if(current_state == PlaybackState::RECORDING){
+                wait_for_frame.wait(lock);
+            }else{
+                lock.unlock();
+                break;
+            }
+        }
+
+        index.playhead = std::to_string(std::chrono::duration_cast<std::chrono::milliseconds> (std::chrono::steady_clock::now() - start_time));
+        flatbuffers::FlatBufferBuilder builder(1024);
+        // auto frame_vector = builder.CreateVector(frame_queue.front());
+        // auto frame = CreateFrame(builder, &frame_vector);
+        // builder.Finish(frame);
+
+        db->Put(leveldb::WriteOptions(), index.playhead, (const char*) builder.GetBufferPointer());
+        frame_queue.pop();
+        BOOST_LOG_TRIVIAL(debug) << "Record frame at: " << index.playhead;
+    }
+
+}
+
+/**
+ * @brief Playback loop function
+ * @details Loops through the leveldb file with an iterator
+ */
+void Playback::play_loop(){
+    boost::unique_lock<boost::mutex> lock(frame_mutex);
     leveldb::Iterator* it = db->NewIterator(leveldb::ReadOptions());
-    while((loop && current_state == State.PLAYING)){
-        for (it->Seek(1); current_state == State.PLAYING, it->Valid() && it->key().ToString() < limit; it->Next()) {
-            pause_condition_variable.wait(lock);
-            callback(it->value());
+    while((loop && current_state == PlaybackState::PLAYING)){
+        for (it->Seek(index.playhead); current_state == PlaybackState::PLAYING, it->Valid(); it->Next()) {
+            
+            auto frame = GetFrame(it->value().ToString());
+            callback(frame->pixels());
+            index.playhead = it->key().ToString();
+            BOOST_LOG_TRIVIAL(debug) << "Play frame at: " << index.playhead;
         }
     }
     delete it;
--- src/Playback.h
+++ src/Playback.h
@@ -1,33 +1,62 @@
 #ifndef __Playback_H__
 #define __Playback_H__
-#include <fstream>
 
-enum State {
+#include <fstream>
+#include <chrono>
+#include <vector>
+#include <queue>
+#include <string>
+
+#include <boost/log/trivial.hpp>
+#include <boost/log/core.hpp>
+#include <boost/log/expressions.hpp>
+
+#include "LEDStrip.h"
+#include "leveldb/db.h"
+#include "frame_generated.h"
+
+using namespace Frame;
+using namespace boost::log;
+namespace logging = boost::log;
+
+enum class PlaybackState {
     STOPPED,
     PAUSED,
     PLAYING,
-    CUE_RECORD
-    RECORD_WAIT
+    RECORDING
+};
+
+struct Index{
+    unsigned int chapter;
+    std::string playhead;
 };
 
 class Playback {
     public:
-        Playback(void) {}
+        Playback(std::string file_name);
+        ~Playback();
         void record();
         void play();
+        void stop();
         void pause();
-        void next();
-        void loop(bool t_loop);
-        State get_state();
-        unsigned int get_index();
-        unsigned int get_total();
-        double get_size(unsigned int index);
+        void toggle_loop(bool t_loop);
+        std::queue<std::vector<Pixel>> frame_queue;
+        void set_state(PlaybackState state);
     private:
-        std::function<void(std::vector<Pixel>&)> callback;
-        State current_state;
-        leveldb::DB* db;
-        int framerate
+        mutable boost::mutex frame_mutex;
+        mutable boost::mutex state_mutex;
+        void play_loop();
+        void record_loop();
         bool loop;
+        Index index;
+        int start_time;
+        int playhead;
+        std::function<void(std::vector<Pixel>&)> callback;
+        PlaybackState current_state;
+        leveldb::DB* db;
+        boost::thread* record_thread;
+        boost::thread* playback_thread;
+        boost::condition_variable wait_for_frame;
 };
 
 #endif /* __Playback_H__ */
