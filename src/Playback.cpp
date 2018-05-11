d65fa3c8804235572cb901d4222943fd7d880911
Grant Harris <grant@grantharris.ca>
2018-05-10 17:57:25 -0700

    save

--- src/Playback.cpp
+++ src/Playback.cpp
@@ -25,7 +25,7 @@ void Playback::push_frame(std::vector<Pixel> const& t_pixels){
     
     nzr::Pixel* pixel = frame.add_pixels();
     
-    for(std::vector<Pixel>::iterator it = t_pixels->begin(); it != t_pixels->end(); it++){
+    for(std::vector<Pixel>::iterator it = t_pixels.begin(); it != t_pixels.end(); it++){
         pixel->set_r(*it.r);
         BOOST_LOG_TRIVIAL(debug) << "Set r: " << *it.r;
         pixel->set_g(*it.g);
